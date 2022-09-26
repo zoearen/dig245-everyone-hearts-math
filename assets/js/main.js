409949b2b10c1b9e5208ee0af34bb5dd21c08508
Zoe Ren <zoren@davidson.edu>
2022-09-26 00:28:13 -0400

    final edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
 
 /* javascript */
+
+//$("h1").hide();
+let number1 = 0;
+let number2 = 0;
+
+$('form').submit(function(e) {
+  e.preventDefault();
+  number1 = $(".number1").val();
+  console.log(number1);
+});
+
+$('form').submit(function(e) {
+  e.preventDefault();
+  number2 = $(".number2").val();
+  console.log(number2);
+});
+
+
+// - when user input n (number)
+// current apples ()
+
+ //
